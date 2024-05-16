cdda30b39a64d743236658170359630ede7404c3
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-05-16 22:31:13 +0100

    added: outputs.tf

--- main.tf
+++ main.tf
@@ -77,16 +77,3 @@ resource "aws_security_group" "crystall_sg" {
     }
 }
 
-output "instance_id" {
-  value       = "aws_instance.test_ubuntu_nginx[count.index].id"
-  sensitive   = true
-  description = "description"
-  depends_on  = []
-}
-
-output "instance_public_ip" {
-  value       = "aws_eip.my_static_ip[count.index].public_ip"
-  sensitive   = true
-  description = "description"
-  depends_on  = []
-}
