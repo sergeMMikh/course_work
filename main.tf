3b605b0a4762038710c9ca2cc9e1d5cd976828e9
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-05-16 22:22:47 +0100

    Added: outputs

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ resource "aws_instance" "test_ubuntu_nginx" {
         project_name    = "Course Work. ",
         description     = "description",
         owner           = "SMMikh"
-    })
-
-    
+    })    
 
     tags = {
       Name = "Backend"
@@ -27,6 +25,7 @@ resource "aws_instance" "test_ubuntu_nginx" {
 
     lifecycle {    
         create_before_destroy = true  
+        ignore_changes = [user_data]
     }
 }
 
@@ -78,3 +77,16 @@ resource "aws_security_group" "crystall_sg" {
     }
 }
 
+output "instance_id" {
+  value       = "aws_instance.test_ubuntu_nginx[count.index].id"
+  sensitive   = true
+  description = "description"
+  depends_on  = []
+}
+
+output "instance_public_ip" {
+  value       = "aws_eip.my_static_ip[count.index].public_ip"
+  sensitive   = true
+  description = "description"
+  depends_on  = []
+}
