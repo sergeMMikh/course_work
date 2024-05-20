4899f084f252b81644c40b72aca25992bbdd3b8b
smmih <s.m.mih@mail.ru>
2024-05-20 19:36:10 +0100

    added: data.tf and limited sequrity group for the internal net

--- main.tf
+++ main.tf
@@ -1,159 +1,149 @@
-# Get a latest ubuntu version in current region
-data "aws_ami" "latest_ubuntu"{
-    owners = ["099720109477"]
-    most_recent = true
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
-    }
-}
-
 resource "aws_instance" "nginx_srv" {
-    count           = 2
-    # ami             = "ami-099b7bab1b9843525" # Amazon Linux AMI
-    ami             = data.aws_ami.latest_ubuntu.id
-    instance_type   = "t4g.micro"
-    
-    key_name        = "mikhalev@DEM-PC1048"
+  count = 2
+  # ami             = "ami-099b7bab1b9843525" # Amazon Linux AMI
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t4g.micro"
 
-    vpc_security_group_ids = [
-        aws_security_group.external_net.id
-    ]
-    
-    user_data       = templatefile("userdata.tpl",
+  key_name = "mikhalev@DEM-PC1048"
+
+  vpc_security_group_ids = [
+    aws_security_group.external_net.id
+  ]
+
+  user_data = templatefile("userdata.tpl",
     {
-        project_name    = "Course Work. ",
-        description     = "description",
-        owner           = "SMMikh"
-    })    
+      project_name = "Course Work. ",
+      description  = "description",
+      owner        = "SMMikh"
+  })
 
-    tags = {
-      Name = "Backend"
-      Owner = "SMMikh"
-      Project = "Course Work. DevOps Engineer."
-    }
+  tags = {
+    Name    = "Backend"
+    Owner   = "SMMikh"
+    Project = "Course Work. DevOps Engineer."
+  }
 
-    lifecycle {    
-        create_before_destroy = true  
-        ignore_changes = [user_data]
-    }
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [user_data]
+  }
 }
 
 resource "aws_instance" "Zabbix_srv" {
-    count           = 1
-    ami             = data.aws_ami.latest_ubuntu.id
-    instance_type   = "t4g.micro"
-    
-    key_name        = "mikhalev@DEM-PC1048"
+  count         = 1
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t4g.micro"
 
-    vpc_security_group_ids = [
-        aws_security_group.external_net.id
-    ]
-    
-    user_data       = templatefile("userdata_zabbix.tpl",
-    {})    
+  key_name = "mikhalev@DEM-PC1048"
 
-    tags = {
-      Name = "Zabbix"
-      Owner = "SMMikh"
-      Project = "Course Work. DevOps Engineer."
-    }
+  vpc_security_group_ids = [
+    aws_security_group.internal_net.id
+  ]
 
-    lifecycle {    
-        create_before_destroy = true  
-        ignore_changes = [user_data]
-    }
+  user_data = templatefile("userdata_zabbix.tpl",
+  {})
+
+  tags = {
+    Name    = "Zabbix"
+    Owner   = "SMMikh"
+    Project = "Course Work. DevOps Engineer."
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [user_data]
+  }
 }
 
 resource "aws_instance" "Elasticsearch_srv" {
-    count           = 1
-    ami             = data.aws_ami.latest_ubuntu.id
-    instance_type   = "t4g.micro"
-    
-    key_name        = "mikhalev@DEM-PC1048"
+  count         = 1
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t4g.micro"
 
-    vpc_security_group_ids = [
-        aws_security_group.external_net.id
-    ]
-    
-    user_data       = templatefile("userdata_elasticsearch.tpl",
-    {})    
+  key_name = "mikhalev@DEM-PC1048"
 
-    tags = {
-      Name = "Elasticsearch"
-      Owner = "SMMikh"
-      Project = "Course Work. DevOps Engineer."
-    }
+  vpc_security_group_ids = [
+    aws_security_group.internal_net.id
+  ]
 
-    lifecycle {    
-        create_before_destroy = true  
-        ignore_changes = [user_data]
-    }
+  user_data = templatefile("userdata_elasticsearch.tpl",
+  {})
+
+  tags = {
+    Name    = "Elasticsearch"
+    Owner   = "SMMikh"
+    Project = "Course Work. DevOps Engineer."
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [user_data]
+  }
 }
 
 resource "aws_instance" "Kibana_srv" {
-    count           = 1
-    ami             = data.aws_ami.latest_ubuntu.id
-    instance_type   = "t4g.micro"
-    
-    key_name        = "mikhalev@DEM-PC1048"
+  count         = 1
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t4g.micro"
 
-    vpc_security_group_ids = [
-        aws_security_group.external_net.id
-    ]
-    
-    user_data       = templatefile("userdata_kibana.tpl",
-    {})    
+  key_name = "mikhalev@DEM-PC1048"
 
-    tags = {
-      Name = "Kibana"
-      Owner = "SMMikh"
-      Project = "Course Work. DevOps Engineer."
-    }
+  vpc_security_group_ids = [
+    aws_security_group.external_net.id
+  ]
 
-    lifecycle {    
-        create_before_destroy = true  
-        ignore_changes = [user_data]
-    }
+  user_data = templatefile("userdata_kibana.tpl",
+  {})
+
+  tags = {
+    Name    = "Kibana"
+    Owner   = "SMMikh"
+    Project = "Course Work. DevOps Engineer."
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [user_data]
+  }
 }
 
 resource "aws_instance" "Bastion_srv" {
-    count           = 1
-    ami             = data.aws_ami.latest_ubuntu.id
-    instance_type   = "t4g.micro"
-    
-    key_name        = "mikhalev@DEM-PC1048"
+  count         = 1
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t4g.micro"
 
-    vpc_security_group_ids = [
-        aws_security_group.external_net.id
-    ]
-    
-    user_data       = templatefile("userdata_bastion.tpl",
-    {})    
+  key_name = "mikhalev@DEM-PC1048"
 
-    tags = {
-      Name = "Bastion"
-      Owner = "SMMikh"
-      Project = "Course Work. DevOps Engineer."
-    }
+  vpc_security_group_ids = [
+    aws_security_group.external_net.id
+  ]
 
-    lifecycle {    
-        create_before_destroy = true  
-        ignore_changes = [user_data]
-    }
+  user_data = templatefile("userdata_bastion.tpl",
+  {})
 
-    depends_on = [
-        aws_instance.nginx_srv,
-        aws_instance.Zabbix_srv,
-        aws_instance.Elasticsearch_srv,
-        aws_instance.Kibana_srv
-    ]
+  tags = {
+    Name    = "Bastion"
+    Owner   = "SMMikh"
+    Project = "Course Work. DevOps Engineer."
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [user_data]
+  }
+
+  depends_on = [
+    aws_instance.nginx_srv,
+    aws_instance.Zabbix_srv,
+    aws_instance.Elasticsearch_srv,
+    aws_instance.Kibana_srv
+  ]
 }
 
 # Elastic IP
 resource "aws_eip" "base_static_ip" {
-    count    = 2
-    instance = aws_instance.nginx_srv[count.index].id
+  count    = 2
+  instance = aws_instance.nginx_srv[count.index].id
 }
 
 ## This can be used for generating a new key pair.
